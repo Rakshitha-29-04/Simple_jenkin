6eb25fc85ea81cad7892d42d4d8c732569fe57cc
Rakshitha-29-04 <rakshithamahadev29@gmail.com>
2026-02-24 14:11:16 +0530

    Create Demo.py

---
+++ Demo.py
@@ -0,0 +1,14 @@
+# Fibonacci series up to n terms
+
+n = 10   # number of terms
+
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
